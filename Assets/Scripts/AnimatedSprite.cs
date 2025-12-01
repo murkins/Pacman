f0e4d42d932e83dd37592785dda7e079fa5f5a44
Kate Haravaya <kateazhevskaya@gmail.com>
2025-12-01 17:03:22 +0100

    added animation for Pacman

--- Assets/Scripts/AnimatedSprite.cs
+++ Assets/Scripts/AnimatedSprite.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
+
 public class AnimatedSprite : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public SpriteRenderer spriteRenderer {get; private set;}
+    public Sprite[] sprites;
+   
+    public float animationTime = 0.25f;
+    
+    public int animationFrame {get; private set; }
+
+    public bool loop = true;
+
+    private void Awake()
     {
-        
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        InvokeRepeating(nameof(Advance), this.animationTime, this.animationTime);
     }
+
+    private void Advance()
+    {
+        if (!this.spriteRenderer.enabled)
+        {
+            return;
+        }
+        
+        this.animationFrame++;
+        if (this.animationFrame >= this.sprites.Length && this.loop)
+        {
+            this.animationFrame = 0;
+        }
+
+        if (this.animationFrame >= 0 && this.animationFrame < this.sprites.Length)
+        {
+            this.spriteRenderer.sprite = this.sprites[this.animationFrame];
+        }
+    }
+
+    public void Restart()
+    {
+        this.animationFrame = -1;
+        Advance();
+    }
+    
 }
